681aea3ebee6d805a74341b5ee080e4f315e8e83
arazel123 <123429409+arazel123@users.noreply.github.com>
2025-05-13 22:13:32 +0330

    Update trader_bot.py

--- trader_bot.py
+++ trader_bot.py
@@ -1,4 +1,4 @@
-API_KEY = 'YOUR_BINANCE_API_KEY'
-API_SECRET = 'YOUR_BINANCE_SECRET_KEY'
-SIGNAL_PAGE_URL = 'https://example.com/signal-page'
+API_KEY = '6d6a40c58b03f5b8dfa954f8cc6acca6851d8c03c656ba6cd2b03e3248359d01'
+API_SECRET = '834e096d6390b9b15cd6dbcd120c74363845ae7dd9b2b95fccbaece7bcc0bcb2'
+SIGNAL_PAGE_URL = 'https://arazel123.github.io/my-webstrategy/'
 
